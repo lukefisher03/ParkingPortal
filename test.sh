31423ec68a7c59f0983c6b5e2868be97c83a71fb
Luke Fisher <lukedavidfisher03@gmail.com>
2024-06-25 01:52:13 -0400

    update gitignore, remove extra files

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
- curl -H "Content-Type: application/json" -X POST http://127.0.0.1:8000/accounts/signup -d @utils/sample_signup.json
- curl -H "Content-Type: application/json" -X POST http://127.0.0.1:8000/accounts/login -d @utils/sample_login.json
