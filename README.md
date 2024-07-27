0341b6ba5f21537dcaa07b7121e0fa19c35468f0
Luke Fisher <lukedavidfisher03@gmail.com>
2024-07-27 16:19:25 -0400

    error handling for email client

--- README.md
+++ README.md
@@ -39,6 +39,16 @@ If you navigate to [`http://localhost:3000/accounts/signup`](http://localhost:30
 ### Notes about email integration
 This app relies on an SMTP connection to a mail client. You will need to set up an email account and enable SMTP Auth in order for your user to be able to manage notifications.
 
+If you do have an email account with an SMTP client then provide it in a file named `credentials.json` in the `/api/` directory like so:
+```
+{
+    "email": "your-awesome-email@coolsite.com",
+    "password": "password"
+}
+```
+
+Note: If you're using outlook you will need to enable two factor authentication and set up an [application password](https://support.microsoft.com/en-us/account-billing/how-to-get-and-use-app-passwords-5896ed9b-4263-e681-128a-a6f2979a7944). 
+
 # UI screenshots
 ![Screenshot of dashboard page](assets/Dashboard.png)
 ![Screenshot of dashboard page](assets/Login.png)
--- api/smtp_email.py
+++ api/smtp_email.py
@@ -9,8 +9,16 @@ def authenticate_smtp():
     """
     Return an authenticated SMTP object to send mail from
     """
-    with open("credentials.json", "r") as f:
-        creds = json.load(f)
+    creds = {}
+    try:
+        with open("credentials.json", "r") as f:
+            creds = json.load(f)
+    except FileNotFoundError:
+        print("Credentials file for email client does not exist")
+
+    if not creds.get("email") or not creds.get("password"):
+        print("There is no credentials provided for the email client, email functionality will not work")
+        return None
     email, password = creds.get("email"), creds.get("password")
     context = ssl.create_default_context()
     server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
@@ -23,18 +31,21 @@ def send_notification_emails(vehicles_to_notify, to):
     msg: [body str, subject]
     to: str
     """
-    with authenticate_smtp() as server:
-        for vehicle in vehicles_to_notify:
-            email_body = NotificationEmail(vehicle)
-            mime_text = MIMEText(email_body.build_email())
-            mime_text["Subject"] = f"CITATION ALERT for {vehicle.get("plate")}"
-            mime_text["From"] = server.user
+    try:
+        with authenticate_smtp() as server:
+            for vehicle in vehicles_to_notify:
+                email_body = NotificationEmail(vehicle)
+                mime_text = MIMEText(email_body.build_email())
+                mime_text["Subject"] = f"CITATION ALERT for {vehicle.get("plate")}"
+                mime_text["From"] = server.user
 
-            errors = server.sendmail(server.user, to, mime_text.as_string())
-            if errors:
-                print(f"There were errors sending your email, {errors}")
-            else:
-                print("Email was sent!!")
+                errors = server.sendmail(server.user, to, mime_text.as_string())
+                if errors:
+                    print(f"There were errors sending your email, {errors}")
+                else:
+                    print("Email was sent!!")
+    except TypeError as e:
+        print("UI tried to trigger an email sending, there's no credentials provided for the SMTP connection. Please see readme for setting up email integration.")
 
 if __name__ == "__main__":
     mail_server = authenticate_smtp()
